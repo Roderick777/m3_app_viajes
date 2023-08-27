46ce0de71c8d45eac165c47522e6827e069a4281
Rodrigo Alexis González Covarrubias <rodrigoalexisgonzalezcovarrubias@MacBook-Pro-de-Rodrigo.local>
2023-08-26 22:50:54 -0400

    Add FadeInAnimation

--- lib/ui/views/home_view/home.view.dart
+++ lib/ui/views/home_view/home.view.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:m3_app_viajes/data/services/place.service.dart';
@@ -43,58 +44,61 @@ class _HomeViewState extends State<HomeView> {
         );
       },
       title: const Text('Travel App'),
-      child: Stack(
-        children: [
-          SizedBox(
-            height: size.height,
-            width: size.width,
-            child: isMainPlatform() ? const Text('') : CustomPaint(painter: HeaderPaintWaves()),
-          ),
-          SizedBox(
-            width: size.width,
-            height: size.height,
-            child: ListView.builder(
-              itemCount: _places.length,
-              itemBuilder: (BuildContext c, int i) {
-                PlaceModel element = _places[i];
-
-                if (element.title == '' && element.description == '') {
-                  return SizedBox(
-                    height: 130,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 10, right: 10, top: 30),
-                      child: Wrap(
-                        spacing: 3,
-                        children: [
-                          Text(
-                            'Travel App',
-                            style: GoogleFonts.dancingScript(
-                              fontSize: 40,
-                              color: isMainPlatform() ? Colors.blue : Colors.white,
-                            ),
-                          ),
-                          const Icon(Icons.sunny, color: Colors.amber),
-                        ],
-                      ),
-                    ),
-                  );
-                } else {
-                  return PlaceCardComponent(
-                    place: element,
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => DetailView(place: element),
-                        ),
-                      );
-                    },
-                  );
-                }
-              },
+      child: FadeInLeft(
+        duration: const Duration(milliseconds: 1000),
+        child: Stack(
+          children: [
+            SizedBox(
+              height: size.height,
+              width: size.width,
+              child: isMainPlatform() ? const Text('') : CustomPaint(painter: HeaderPaintWaves()),
             ),
-          ),
-        ],
+            SizedBox(
+              width: size.width,
+              height: size.height,
+              child: ListView.builder(
+                itemCount: _places.length,
+                itemBuilder: (BuildContext c, int i) {
+                  PlaceModel element = _places[i];
+
+                  if (element.title == '' && element.description == '') {
+                    return SizedBox(
+                      height: 130,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 10, top: 30),
+                        child: Wrap(
+                          spacing: 3,
+                          children: [
+                            Text(
+                              'Travel App',
+                              style: GoogleFonts.dancingScript(
+                                fontSize: 40,
+                                color: isMainPlatform() ? Colors.blue : Colors.white,
+                              ),
+                            ),
+                            const Icon(Icons.sunny, color: Colors.amber),
+                          ],
+                        ),
+                      ),
+                    );
+                  } else {
+                    return PlaceCardComponent(
+                      place: element,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailView(place: element),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
